f53d389130f26a11a9dae5818a9658417bafc32f
dmoon <dmoon.t@gmail.com>
2017-06-17 16:20:09 +0800

    Refactor redux configureStore and reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
-import { combineReducers } from 'redux';
-import counter from './counter';
-import nav from './nav';
+// import { combineReducers } from 'redux';
+// import counter from './counter';
+// import nav from './nav';
 
-export default combineReducers({
-  counter,
-  nav
-});
+// export default combineReducers({
+//   counter,
+//   nav
+// });
+
+export * from "./counter";
+export * from "./nav";
--- src/reducers/nav.js
+++ src/reducers/nav.js
@@ -1,9 +1,11 @@
-import AppNavigator from '../navigator';
+import AppNavigator from "../navigator";
 
 const initialState = {
   index: 0,
-  routes: [{ key: 'Init', routeName: 'Home' }]
+  routes: [{ key: "Init", routeName: "Home" }]
 };
 
-export default (state = initialState, action) =>
+const nav = (state = initialState, action) =>
   AppNavigator.router.getStateForAction(action, state);
+
+export { nav };
